ac765f5ac0eddb37ae095aee740cb7cf379eac9e
torn2706 <torn0627@gmail.com>
2024-01-31 20:34:54 +0100

    Input System imported

---
+++ DriftDemo/Assets/Scripts/Infrastructure/Classes/GameplayEntryPoint.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Infrastructure
+{
+    public class GameplayEntryPoint : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+    }
+}
+
