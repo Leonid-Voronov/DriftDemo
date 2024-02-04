bdd2b91289ffc557f30fbeec042cec51c4f8af55
torn2706 <torn0627@gmail.com>
2024-02-04 02:42:34 +0100

    Car purchases wrap up

--- DriftDemo/Assets/Scripts/UI/MainMenu/Panels/ShopPanel.cs
+++ DriftDemo/Assets/Scripts/UI/MainMenu/Panels/ShopPanel.cs
@@ -11,19 +11,39 @@ namespace UI.MainMenu
         [SerializeField] private Button _car1Button;
         [SerializeField] private Button _car2Button;
         private IPlayerDataService _playerDataService;
+        private PlayerGarage _playerGarage;
 
         [Inject]
         public void Construct(IPlayerDataService playerDataService)
         {
             _playerDataService = playerDataService;
+            _playerGarage = playerDataService.PlayerGarage;
         }
 
         private void OnEnable()
         {
             InitButton(_car1Button, CarName.Car1);
             InitButton(_car2Button, CarName.Car2);
+
+            _car1Button.onClick.AddListener(OnCar1ButtonPressed);
+            _car2Button.onClick.AddListener(OnCar2ButtonPressed);
         }
 
+        private void OnCar1ButtonPressed() => OnPurchaseButtonPressed(_car1Button, CarName.Car1);
+        private void OnCar2ButtonPressed() => OnPurchaseButtonPressed(_car2Button, CarName.Car2);
+
         private void InitButton(Button button, CarName carName) => button.interactable = !_playerDataService.PlayerGarage.IsCarPurchased(carName);
+
+        private void OnPurchaseButtonPressed(Button button, CarName carName)
+        {
+            if (_playerGarage.BuyCar(carName))
+                button.interactable = false;
+        }
+
+        private void OnDisable()
+        {
+            _car1Button.onClick.RemoveListener(OnCar1ButtonPressed);
+            _car2Button.onClick.RemoveListener(OnCar2ButtonPressed);
+        }
     }
 }
