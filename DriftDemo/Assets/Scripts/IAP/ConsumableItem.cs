fa38b3deb244a53c3c4ae1f4eb6b6fa95411e46d
torn2706 <torn0627@gmail.com>
2024-02-05 08:58:53 +0100

    Car purchasing fix

---
+++ DriftDemo/Assets/Scripts/IAP/ConsumableItem.cs
@@ -0,0 +1,25 @@
+﻿namespace Purchases
+{
+    [System.Serializable]
+    public class ConsumableItem
+    {
+        private string _name;
+        private string _id;
+        private string _description;
+        private float _price;
+
+        public ConsumableItem(string name, string id, string description, float price)
+        {
+            _name = name;
+            _id = id;
+            _description = description;
+            _price = price;
+        }
+
+        public string Name => _name;
+        public string Id => _id;
+        public string Description => _description;
+        public float Price => _price;
+    }
+}
+
